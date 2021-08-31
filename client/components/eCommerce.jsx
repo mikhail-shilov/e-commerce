7e2ba17bc29d459821e7486ab6f046166aafe772
Mikhail Shilov <mih.shilov@gmail.com>
2021-08-31 23:38:47 +0300

    Let's go

---
+++ client/components/eCommerce.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from './head'
+
+const eCommerce = () => (
+  <div>
+    <Head title="Catalog" />
+    <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+      All will be here!
+    </div>
+  </div>
+)
+
+export default eCommerce
