10947a27809a7b2b7fc352791e8053b600d663ef
Mikhail Shilov <mih.shilov@gmail.com>
2021-09-06 22:44:41 +0300

    First redux integrate

---
+++ client/redux/reducers/currency.js
@@ -0,0 +1,13 @@
+const initialState = {
+  date: null,
+  USD: 1,
+  EUR: null,
+  CAD: null
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
---
+++ client/redux/reducers/goods.js
@@ -0,0 +1,13 @@
+const initialState = {
+  goodsOnPage: 12,
+  currentPage: 1,
+  goodsList: [],
+  test: 42
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
