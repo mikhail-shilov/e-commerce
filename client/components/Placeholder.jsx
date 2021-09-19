1cde494058048e8e8b2ffa86fe212e45271f8a44
Mikhail Shilov <mih.shilov@gmail.com>
2021-09-19 11:43:16 +0300

    Add placeholder

---
+++ client/components/Placeholder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Placeholder = () => (
+  <div className="flex-grow h-full w-full flex text-center justify-center items-center mt-4 mx-20 px-2 py-2">
+    <h2 className="text-lg">Loading...</h2>
+  </div>
+)
+
+export default Placeholder
