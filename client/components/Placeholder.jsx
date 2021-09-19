c1dcf6ff7259a7feecdbd9956d8c5a75753f1209
Mikhail Shilov <mih.shilov@gmail.com>
2021-09-19 13:02:11 +0300

    Fix placeholder

--- client/components/Placeholder.jsx
+++ client/components/Placeholder.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Placeholder = () => (
-  <div className="flex-grow h-full w-full flex text-center justify-center items-center mt-4 mx-20 px-2 py-2">
+  <div className="flex-grow h-full w-full flex justify-center items-center mt-4 mx-20 px-2 py-2">
     <h2 className="text-lg">Loading...</h2>
   </div>
 )
