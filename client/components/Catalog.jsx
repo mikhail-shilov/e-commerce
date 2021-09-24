ec64311fa5baa76bc975035d15366715c21b9c78
Mikhail Shilov <mih.shilov@gmail.com>
2021-09-24 19:06:52 +0300

    Move catalog logic from head component eCommerce to Catalog

--- client/components/Catalog.jsx
+++ client/components/Catalog.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+
 import GoodCard from './catalog/GoodCard'
 import Pagination from './catalog/Pagination'
 import SubHeader from './SubHeader'
 import Placeholder from './Placeholder'
-import { changeSortMode, setDescendingOrder } from '../redux/reducers/catalog'
+import {
+  changeSortMode,
+  setDescendingOrder,
+  setLoadingCatalog,
+  updateGoods
+} from '../redux/reducers/catalog'
 
 const Catalog = () => {
+  const history = useHistory()
+  const dispatch = useDispatch()
+
   const rates = useSelector((state) => state.currency.rates)
   const activeCurrency = useSelector((state) => state.currency.activeCurrency)
-  const goods = useSelector((state) => state.catalog.goods)
+
+  const onPage = useSelector((state) => state.catalog.onPage)
+  const page = useSelector((state) => state.catalog.page)
+  const sort = useSelector((state) => state.catalog.sorting)
+
   const isLoadingCatalog = useSelector((state) => state.catalog.isLoading)
+  const goods = useSelector((state) => state.catalog.goods)
   const basketLocal = useSelector((state) => state.basket.basketLocal)
+
+  useEffect(() => {
+    dispatch(setLoadingCatalog(true))
+    const query = `/api/v1/goods/${page}?onpage=${onPage}&sort=${sort.mode}&desc=${sort.isDescOrder}`
+    axios.get(query).then((result) => {
+      dispatch(updateGoods(result.data.data.goods, result.data.data.pages))
+      dispatch(setLoadingCatalog(false))
+    })
+  }, [page, sort.mode, sort.isDescOrder])
+
+  useEffect(() => {
+    history.push(`/${page}`)
+    console.log(`Effect by page: ${page}`)
+  }, [page])
+
 
   const goodCards = goods.map(good => (
     <GoodCard
@@ -27,7 +58,7 @@ const Catalog = () => {
 
   return (
     <div className="main flex flex-col h-100 w-full h-screen px-14 text-center">
-      <SubHeader title='Goods:' acSortMode={changeSortMode} acOrder={setDescendingOrder}/>
+      <SubHeader title='Goods:' acSortMode={changeSortMode} acOrder={setDescendingOrder} />
       {!isLoadingCatalog && <Pagination />}
       <div className='list-of-goods flex w-full flex-grow content-start pb-4 px-20 flex-wrap'>
         {isLoadingCatalog ? <Placeholder /> : goodCards}
--- client/components/eCommerce.jsx
+++ client/components/eCommerce.jsx
@@ -1,45 +1,27 @@
 import React, { useEffect } from 'react'
-import { Route, Switch, useHistory } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 
 import { updateRates } from '../redux/reducers/currency'
-import { setLoadingCatalog, updateGoods } from '../redux/reducers/catalog'
 import { setLoadingBasket, updateBasket } from '../redux/reducers/basket'
 
-import Head from './head'
 import NotFound from './404'
+import Head from './head'
 import Header from './Header'
+
 import Catalog from './Catalog'
 import Basket from './Basket'
 
 const ECommerce = () => {
   const dispatch = useDispatch()
-  const history = useHistory()
   const basketLocal = useSelector((state) => state.basket.basketLocal)
-  const onPage = useSelector((state) => state.catalog.onPage)
-  const page = useSelector((state) => state.catalog.page)
-  const sort = useSelector((state) => state.catalog.sorting)
 
   useEffect(() => {
     axios.get('/api/v1/rate').then((result) => {
       dispatch(updateRates(result.data.data.rates))
     })
   }, [])
-
-  useEffect(() => {
-    history.push(`/${page}`)
-    console.log(`Effect by page: ${page}`)
-  }, [page])
-
-  useEffect(() => {
-    dispatch(setLoadingCatalog(true))
-    const query = `/api/v1/goods/${page}?onpage=${onPage}&sort=${sort.mode}&desc=${sort.isDescOrder}`
-    axios.get(query).then((result) => {
-      dispatch(updateGoods(result.data.data.goods, result.data.data.pages))
-      dispatch(setLoadingCatalog(false))
-    })
-  }, [page, sort.mode, sort.isDescOrder])
 
   useEffect(() => {
     dispatch(setLoadingBasket(true))
